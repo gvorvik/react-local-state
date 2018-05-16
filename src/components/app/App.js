39c1fabf209795543e05fb146daecd2e892a749f
Greg Orvik <gvorvik@gmail.com>
2018-05-16 17:31:50 -0500

    append entries to the dom in a list

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,59 +1,91 @@
 import React, { Component } from 'react';
 
+const emptyUser = {
+  name: '',
+  city: '',
+  zip: '',
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      user: {
-        name: 'Greg',
-        city: 'Minneapolis'
-      }
+      user: emptyUser,
+
+      userList: [],
+
     };
 
+    // All comments below refer to line with ***  ***
     //makes 'this' in handleChange the same as
     //'this' in the constructor
     //similar to const self = this
-    // this.handleChange = this.handleChange.bind(this);
+    //If we write the functions like they are below (e.g. handleChange) we don't need the line below (this.handleChange...)
+    // *** this.handleChange = this.handleChange.bind(this);***
   }
 
 
-  handleChange = (event) => {
-    // console.log('input was changed');
-    console.log('event.target', event.target.value);
 
-
-    // this.state.use = event.target.value;
+  //Example of currying (a function that returns a function)
+  //In es6, parenthesis can leave if there is one parameter
+  //If the returned function is on the same line as the parent, it will automatically return the function (without return statement)
+  handleChangeFor = propertyName => event => {
     this.setState({
       user: {
         ...this.state.user,
-        name: event.target.value,
+        [propertyName]: event.target.value,
       }
     });
-  };
+  }
 
-  cityChange = (event) => {
-    console.log('event.target', event.target.value);
+//Function below is same as function above (for reference on notes about es6)
 
-    this.setState({
-      user: {
-        ...this.state.user,
-        city: event.target.value,
-      }
-    });
-  };
+  // handleChangeFor = (propertyName) => {
+  //   return (event) => {
+  //     this.setState({
+  //       user: {
+  //         ...this.state.user,
+  //         [propertyName]: event.target.value,
+  //       }
+  //     });
+  //   }
+  // }
 
-  logIt = () => {
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({userList: [...this.state.userList, this.state.user]});
     console.log(this.state.user);
+    this.setState({
+      user: emptyUser,
+    });
+    console.log(this.state.userList);
   };
+
+
+
+
+
 
   render() {
+
+    let updatedUserList = this.state.userList.map(user => <li key={user.name}>{user.name} is from {user.city}, zip code {user.zip}</li>);
+
     return (
       <div>
-        The current user is {this.state.user.name} and s/he is from {this.state.user.city}
-        <input onChange={this.handleChange} placeholder="Username" />
-        <input onChange={this.cityChange} placeholder="City" />
-        <button onClick={this.logIt}>Submit</button>
+        The current user is {this.state.user.name}. S/he is from {this.state.user.city}, where the zip code is {this.state.user.zip}
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.user.name} onChange={this.handleChangeFor('name')} placeholder="Username" />
+          <input value={this.state.user.city} onChange={this.handleChangeFor('city')} placeholder="City" />
+          <input value={this.state.user.zip} onChange={this.handleChangeFor('zip')} placeholder="Zip Code" />
+          <input type="submit" value="Click me to submit form!" />
+        </form>
+
+        <ul>
+          {updatedUserList}
+        </ul>
       </div>
     );
   }
